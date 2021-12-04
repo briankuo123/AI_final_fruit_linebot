8ac73c6f015a511a3d2dc57902e02c1aa6efa3be
LTM <brian.eh.kuo@gmail.com>
2021-12-04 10:25:14 +0800

    .

--- app.py
+++ app.py
@@ -11,6 +11,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/function')
+def function():
+    return render_template('function.html')
+
+@app.route('/about')
+def function():
+    return render_template('about.html')
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
